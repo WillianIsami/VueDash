3a3d002a0b5e9a7d106478015f45e991d5a6605b
Willian <willianisami1229@gmail.com>
2025-01-10 23:04:36 -0300

    feat(frontend): restructure project and enhance book pages
    - Moved Nuxt files to the `frontend` folder for better organization.
    - Created `popular.vue` to display popular books.
    - Updated `books.vue` to allow users to select similar books by ISBN or recommendations by User_ID.
    - Implemented Intersection Observer for infinite scrolling on `books.vue` and `popular.vue`.
    - Added TailwindCSS for improved styling and layout.

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/" class="nav-link">Home</NuxtLink></li>
+          <li><NuxtLink to="/books" class="nav-link">Books</NuxtLink></li>
+          <li><NuxtLink to="/popular" class="nav-link">Popular Books</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+header {
+  background-color: #0d78b6;
+  padding: 10px 20px;
+}
+
+nav ul {
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+nav li {
+  margin: 0 15px;
+}
+
+.nav-link {
+  color: white;
+  text-decoration: none;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 8px 16px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
+
+.nav-link:hover {
+  background-color: #3498db;
+  color: white;
+}
+
+@media (max-width: 768px) {
+  nav ul {
+    flex-direction: column;
+    align-items: center;
+  }
+  nav li {
+    margin: 10px 0;
+  }
+}
+</style>
